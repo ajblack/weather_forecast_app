47f8ca84a310b5d2e0babddb2e417398be882f9e
Austin Blackman <austin.blackman@gmail.com>
2021-01-05 16:30:54 -0500

    initial weather app commit.  Boston coordinates are hard coded into query

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import WeatherForecastContainer from './WeatherForecastContainer';
+
+function App() {
+  return (
+    <div className="App">
+      <WeatherForecastContainer/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/WeatherForecastContainer.jsx
@@ -0,0 +1,93 @@
+import react, {useEffect, useState} from 'react';
+import './WeatherForecastContainer.css';
+
+export default function WeatherForecastContainer(){
+
+  const [lat, setLat] = useState(null);
+  const [lon, setLon] = useState(null);
+  const [city, setCity] = useState('');
+  const [state, setState]= useState('');
+  const [periods, setPeriods] = useState([]);
+
+  useEffect(()=>{
+    console.log(`lat is: ${lat} and lon is ${lon}`)
+  },[lat,lon])
+
+  useEffect(()=>{
+    console.log(`city is: ${city} and state is ${state}`)
+  },[city,state])
+
+  useEffect(()=>{
+    console.log('periods set:');
+    console.log(periods);
+  },[periods]);
+
+  const resetFields = function(){
+    setLat(null);
+    setLon(null);
+    setCity('');
+    setState('');
+    setPeriods([]);
+  }
+
+  const getWeather = async function(){
+    resetFields();
+    //const forecast_link_info = await fetch(`https://api.weather.gov/points/${lat},${lon}`).then(res=>res.json());
+    const forecast_link_info = await fetch("https://api.weather.gov/points/42.3611,-71.057").then(res=>res.json());
+    if(forecast_link_info.status === 404){
+      alert('please enter valid coordinates');
+    }
+    else{
+      setCity(forecast_link_info.properties.relativeLocation.properties.city);
+      setState(forecast_link_info.properties.relativeLocation.properties.state);
+      const forecast_link = forecast_link_info.properties.forecast;
+      const forecast_json = await fetch(forecast_link).then(res=>res.json());
+      setPeriods(forecast_json.properties.periods);
+
+    }
+  }
+
+
+  return(
+    <div style={{'paddingTop':'30px'}}>
+      <div className="user-inputs">
+          <div className="form-pair">
+            <label htmlFor='lat'>Lat: </label>
+            <input id="lat" type='text' onChange={(e)=>setLat(e.target.value)}/>
+          </div>
+          <div className="form-pair">
+            <label htmlFor='lon' >Lon: </label>
+            <input id="lon" type='text' onChange={(e)=>setLon(e.target.value)}/>
+          </div>
+          <button onClick={getWeather}>Get Forecast</button>
+      </div>
+
+    <div className={city===""|| state==="" ? 'hidden' : ''}>
+      <h2>Weather For {city}, {state}</h2>
+      <ul>
+        {
+          periods.map(period=>{
+            return(
+              <li>
+                <h3>{period.name}</h3>
+                <div className="weather-content-entry">
+                  <p>
+                  <div>
+                    <span>Temp: {period.temperature} {period.temperatureUnit}</span>
+                  </div>
+                  <div>
+                    <span>{period.detailedForecast}</span>
+                  </div>
+                  </p>
+                  <img src={period.icon}/>
+                </div>
+              </li>
+            )
+          })
+        }
+      </ul>
+    </div>
+    </div>
+
+  )
+}
